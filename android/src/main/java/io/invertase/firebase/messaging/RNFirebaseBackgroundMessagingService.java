907fdf139ff2c101d3189df6f8800107c389a5cc
Ryan <dltjdgh23@gmail.com>
2020-03-18 20:00:51 +0900

    fix strange space string

--- android/src/main/java/io/invertase/firebase/messaging/RNFirebaseBackgroundMessagingService.java
+++ android/src/main/java/io/invertase/firebase/messaging/RNFirebaseBackgroundMessagingService.java
@@ -14,18 +14,18 @@ import com.google.firebase.messaging.RemoteMessage;
 import javax.annotation.Nullable;
 ​
 public class RNFirebaseBackgroundMessagingService extends HeadlessJsTaskService {
-​
+	​
   @Override
   protected @Nullable HeadlessJsTaskConfig getTaskConfig(Intent intent) {
-​
+  	​
 		RemoteMessage _message = intent.getParcelableExtra("message");
-​
+		​
 		if (_message.getData().get("message").split(":")[0].equals("call")
 			|| _message.getData().get("message").split(":")[0].equals("call_group")) {
 			Intent callIntent = new Intent(getApplicationContext(), CallingActivity.class);
 			callIntent.setAction(Intent.ACTION_SCREEN_ON);
 			callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-​
+			​
 			Bundle b = new Bundle();
 			b.putString("message", _message.getData().get("message"));
 			callIntent.putExtras(b);

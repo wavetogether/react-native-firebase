48f466cda360e7280f8e1ae25cf33596f3dd3989
Ryan <dltjdgh23@gmail.com>
2020-06-08 22:17:49 +0900

    no message

--- android/src/main/java/io/invertase/firebase/messaging/CallingActivity.java
+++ android/src/main/java/io/invertase/firebase/messaging/CallingActivity.java
@@ -59,13 +59,13 @@ public class CallingActivity extends Activity {
       window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
       window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
       window.setStatusBarColor(Color.TRANSPARENT);
+      window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
-    hideSoftMenuBar();
     setContentView(R.layout.activity_calling);
 
-		int orientation = getResources().getInteger(R.integer.orientation);
-		setRequestedOrientation(orientation);
+    int orientation = getResources().getInteger(R.integer.orientation);
+    setRequestedOrientation(orientation);
 
     TextView tv_title = findViewById(R.id.call_title);
     tv_title.setText(message.split(";")[0]);
@@ -157,15 +157,6 @@ public class CallingActivity extends Activity {
         return true;
     }
     return super.dispatchKeyEvent(event);
-  }
-
-  public void hideSoftMenuBar() {
-    getWindow().getDecorView().setSystemUiVisibility(
-      View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-    );
   }
 
   private void vibrateApp() {
